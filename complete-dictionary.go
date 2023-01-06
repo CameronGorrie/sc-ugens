f36ca5dc361ba60202dbaf825be7fda56bef4317
Cameron Gorrie <cameronwgorrie@gmail.com>
2023-01-06 09:43:45 -0500

    Add simple delay

--- complete-dictionary.go
+++ complete-dictionary.go
@@ -3,8 +3,9 @@ package ugens
 import "github.com/CameronGorrie/sc"
 
 var CompleteDictionary = map[string]sc.UgenFunc{
-	"bpf":         BPF,
-	"mic":         Mic,
-	"simple_saw":  SimpleSaw,
-	"simple_sine": SimpleSine,
+	"bpf":          BPF,
+	"mic":          Mic,
+	"simple_delay": SimpleDelay,
+	"simple_saw":   SimpleSaw,
+	"simple_sine":  SimpleSine,
 }
