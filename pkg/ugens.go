3956a219991dd2495ba17a71e63c67446d52ea51
Cameron Gorrie <cameronwgorrie@gmail.com>
2023-03-25 14:30:36 -0400

    Delete mpe folder and contents

--- pkg/ugens.go
+++ pkg/ugens.go
@@ -3,14 +3,12 @@ package ugens
 import (
 	"github.com/CameronGorrie/sc"
 	"github.com/CameronGorrie/ugens/pkg/ambient"
-	"github.com/CameronGorrie/ugens/pkg/mpe"
 	"github.com/CameronGorrie/ugens/pkg/samples"
 	"github.com/CameronGorrie/ugens/pkg/simple"
 	"github.com/CameronGorrie/ugens/pkg/soundin"
 )
 
 var Lib = map[string]sc.UgenFunc{
-	"bpf":              mpe.BPF,
 	"buf":              samples.Buf,
 	"detuneDistortion": ambient.DetuneDistortion,
 	"mic":              soundin.Mic,
